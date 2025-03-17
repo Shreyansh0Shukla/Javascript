a34ca1055051d9e7a5a5f1e81b1f726e39fd9508
Shreyansh0Shukla <shreyanshshukla.693@gmail.com>
2025-03-17 06:22:21 +0000

    datatypes

---
+++ 01basics/datatype.js
@@ -0,0 +1,16 @@
+"use strict";//treat all JS code as newer version
+//alert(3+3) will work on browser but we are using node.js
+/*
+number=> 2^53
+bigint
+string=>""
+boolean=>true/false
+null=>standalone value
+undefined=>
+symbol=>uniqueness
+object
+*/
+let age=124
+console.log(typeof age)//int
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object 
