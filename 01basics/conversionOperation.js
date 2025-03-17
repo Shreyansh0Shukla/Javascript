20f7b8bdb7e34c82bfc94a9ba9283dfa58da73bc
Shreyansh0Shukla <shreyanshshukla.693@gmail.com>
2025-03-17 06:49:14 +0000

    ConsversionsOperations.

---
+++ 01basics/conversionOperation.js
@@ -0,0 +1,64 @@
+/*
+let score=33
+console.log(typeof score) // number
+
+let a="33"
+console.log(typeof a)// string
+let valueInNumber= Number(a)
+console.log(typeof valueInNumber) // number
+
+let b="33av"
+console.log(typeof b)// string
+let bNumber= Number(b)
+console.log(typeof bNummber) // number
+console.log(bNummber)// NaN
+
+let c=NULL
+console.log(typeof c)// object
+let cNumber= Number(c)
+console.log(typeof cNummber) // number
+console.log(cNummber)// 0
+
+let d=undefined
+console.log(typeof d)// undefined
+let dNumber= Number(d)
+console.log(typeof dNummber) // number
+console.log(dNummber)// NaN
+
+let e=true
+console.log(typeof e)// boolean
+let eNumber= Number(e)
+console.log(typeof eNummber) // number
+console.log(eNummber)// 1
+
+
+let f="Name"
+console.log(typeof f)// undefined
+let fNumber= Number(f)
+console.log(typeof fNummber) // number
+console.log(fNummber)// NaN
+
+
+let n=1
+let bool=Boolean(n)
+crossOriginIsolated.log(bool)//1
+   1=>true, 0=> false
+   "" => false
+   "avdpmv"=>true
+
+let m=35
+let strin= String(m)
+console.log(typeof strin) // string
+*/
+
+//******************************** operation*************
+/*
+console.log("Hi" + "lol")  // Hi lol
+console.log("1"+2) // 12
+console.log(1+"2")//12
+console.log("1"+2+2) // 122
+console.log(1+2+"2")//32
+*/
+let count=0
+console.log(count++)
+console.log(++count)
