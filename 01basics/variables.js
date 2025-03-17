a9a8eb68cfb503416647e87e91eb9760fa9d6ca9
Shreyansh0Shukla <shreyanshshukla.693@gmail.com>
2025-03-17 06:22:34 +0000

    variables

---
+++ 01basics/variables.js
@@ -0,0 +1,12 @@
+const accountID=1234
+let accountEmail="falana@dhikana.com"
+var accountPassword="1234"
+accountCity="lola"
+/*
+ Never use var and also the way accountCity is used
+*/
+console.table([accountID,accountEmail,accountPassword,accountCity])
+accountEmail="lolla@lussan.com"
+accountPassword="4212"
+accountCity="Jhumplitaliya"
+console.table([accountID,accountEmail,accountPassword,accountCity])
