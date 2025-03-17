d557529bc6bb7b627dc73e5a25d1c85e4e12c66a
Shreyansh0Shukla <shreyanshshukla.693@gmail.com>
2025-03-17 07:00:38 +0000

    comparision

---
+++ 01basics/comparisions.js
@@ -0,0 +1,16 @@
+// console.log(2>1)//true
+// console.log("2">1)//true
+//avoid them below one
+// console.log(null>0)//false
+// console.log(null==0)//false
+// console.log(null>=0)//true
+// //the wroking of == operator works differently than >,<,>=,<= 
+
+// console.log(undefined>0)//false
+// console.log(undefined==0)//false
+// console.log(undefined>=0)//false
+// strict check ===
+console.log("2" === 1)
+
+
+
