3b510a94b98e40ca5c8015e85ab28eca56641211
Shreyansh0Shukla <shreyanshshukla.693@gmail.com>
2025-03-18 13:00:04 +0000

    datatype-summary

---
+++ 01basics/variable-summary.js
@@ -0,0 +1,20 @@
+// premitive datatype
+/*
+   string, number, boolean, null,undefined,symbol,BigInt
+*/
+const n=symbol('123')
+const bigNo=12482940358312048n
+//reference(non-premetive)
+/* 
+      Array, Objects, Function
+*/
+const heros=["Shaktiman","Nagraj", "Doga"]
+let obj={
+    name:"shrey",
+    age:22,
+}
+
+function(){}
+const myFunction=function(){      //using function as a variable
+    console.log("Hello World")
+}
