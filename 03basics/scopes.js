fc7f049931605abb2e815287b7fc8f9739c5640b
Shreyansh0Shukla <shreyanshshukla.693@gmail.com>
2025-03-20 09:48:52 +0000

    scopes

---
+++ 03basics/scopes.js
@@ -0,0 +1,10 @@
+
+if(true){
+    let a =10
+    const b=20
+    var c= 30
+}
+//console.log(a)   =>cant be acccesible as out of cope declaration 
+//console.log(b)   =>cant be acccesible as out of cope declaration
+//console.log(c)   => accesible and output generatoed is 30 
+// now suppose someother file is imported and var c values is different than it will efefct the value here
